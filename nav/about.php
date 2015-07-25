c2aa22f78a445d5cc4d657cfd381924f9da300e6
Omayeli Arenyeka <oa495@nyu.edu>
2015-07-25 13:43:57 -0400

    setting up

--- nav/about.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<ul class="b-nav-list otherp">
-            <li><a class ="b-nav-item" href="../art/">Art</a></li>
-            <li><a class ="b-nav-item" href="../photography/">Photography</a></li>
-            <li><a class ="b-nav-item" href="./">About</a></li>
-         </ul>';
-
-echo "\n"; // line break
-?>
--- nav/gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '	 <ul class="b-nav-list galleryp">
-            <li><a class ="b-nav-item galleryp" href="./">Art</a></li>
-            <li><a class ="b-nav-item galleryp" href="../photography/">Photography</a></li>
-            <li><a class ="b-nav-item galleryp" href="../about/">About</a></li>
-         </ul>';
-
-echo "\n"; // line break
-?>
